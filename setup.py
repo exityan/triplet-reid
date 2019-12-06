251c24d828e223de75b45ae65aa3f38171f9676b
exityan <exityan@gmail.com>
2019-12-06 14:05:14 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="triplet-reid",
+    version="0.1.0",
+    description="Triplet-based Person Re-Identification",
+    packages=find_packages(),
+)
